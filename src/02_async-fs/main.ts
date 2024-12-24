da8f9714edc8b9b33ec06f736c44ddb9842dbdd7
Johnny J. Huirilef Millán <johnnyhuirilef@gmail.com>
2024-12-24 11:49:23 -0300

    refactor(fs-async): refactor config loading to enhance error handling and clarity

--- src/02_async-fs/main.ts
+++ src/02_async-fs/main.ts
@@ -1,19 +1,61 @@
 import fs from "node:fs/promises";
 import path from "node:path";
+import { getDefaultPort } from "../utils/index.ts";
 
-// Suponiendo que el archivo se llama config.json y está en el mismo directorio
+const readFileContent = async (filePath: string): Promise<string> => {
+  return await fs.readFile(filePath, "utf-8");
+};
 
-const filePath = path.resolve("./", "assets", "config.json");
-const defaultPort = 3000;
+const parseJson = (data: string): unknown => {
+  return JSON.parse(data);
+};
 
-const readConfig = async () => {
-  const data = await fs.readFile(filePath, "utf-8");
-  const config = JSON.parse(data);
-  const port = typeof config.port === "number" ? config.port : defaultPort;
+const extractPort = (config: unknown) => {
+  if (!config) {
+    return null;
+  }
+
+  if (typeof config !== "object") {
+    return null;
+  }
+
+  if (!("port" in config)) {
+    return null;
+  }
+
+  if (typeof config.port !== "number") {
+    return null;
+  }
+
+  return config.port;
+};
+
+const getFilePath = (filePath: string) => {
+  return path.resolve("./", "assets", filePath);
+};
+
+const getPortFromConfig = async () => {
+  const filePath = getFilePath("config.json");
+  const content = await readFileContent(filePath);
+  if (content === null) {
+    return getDefaultPort();
+  }
+
+  const config = parseJson(content);
+  if (config === null) {
+    return getDefaultPort();
+  }
+
+  const port = extractPort(config) ?? getDefaultPort();
+  return port;
+};
+
+const main = async () => {
+  const port = await getPortFromConfig();
   console.log(`Configured port is: <<<${port}>>>`);
 };
 
-readConfig().catch((error) => {
+main().catch((error) => {
   console.error("Error reading file:", error);
-  console.log(`Using default value: <<<${defaultPort}>>>`);
+  console.log(`Using default value: <<<${getDefaultPort()}>>>`);
 });
