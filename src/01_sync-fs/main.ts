2d25719c7516e5a04ad6d1f6a9ac73f31d1722bf
Johnny J. Huirilef Millán <johnnyhuirilef@gmail.com>
2024-12-24 11:34:01 -0300

    refactor(fs-sync): refactor config handling and improve default port logic

--- src/01_sync-fs/main.ts
+++ src/01_sync-fs/main.ts
@@ -1,21 +1,67 @@
 import fs from "node:fs";
 import path from "node:path";
 
-// Suponiendo que el archivo se llama config.json y está en el mismo directorio
+const getDefaultPort = () => {
+  const DEFAULT_PORT = 3000;
+  return DEFAULT_PORT;
+};
 
-const filePath = path.resolve("./", "assets", "config.json");
-const defaultPort = 3000;
+const readFileContent = (filePath: string): string => {
+  return fs.readFileSync(filePath, "utf-8");
+};
 
-function readConfig() {
+const parseJson = (data: string): unknown => {
+  return JSON.parse(data);
+};
+
+const extractPort = (config: unknown) => {
+  if (!config) {
+    return null;
+  }
+
+  if (typeof config !== "object") {
+    return null;
+  }
+
+  if (!("port" in config)) {
+    return null;
+  }
+
+  if (typeof config.port !== "number") {
+    return null;
+  }
+
+  return config.port;
+};
+
+const getFilePath = (filePath: string) => {
+  return path.resolve("./", "assets", filePath);
+};
+
+const getPortFromConfig = () => {
+  const filePath = getFilePath("config.json");
+  const content = readFileContent(filePath);
+  if (content === null) {
+    return getDefaultPort();
+  }
+
+  const config = parseJson(content);
+  if (config === null) {
+    return getDefaultPort();
+  }
+
+  const port = extractPort(config) ?? getDefaultPort();
+  return port;
+};
+
+const main = () => {
   try {
-    const data = fs.readFileSync(filePath, "utf-8");
-    const config = JSON.parse(data);
-    const port = typeof config.port === "number" ? config.port : defaultPort;
+    const port = getPortFromConfig();
     console.log(`Configured port is: <<<${port}>>>`);
   } catch (error) {
     console.error("Error reading file or parsing JSON:", error);
-    console.log(`Using default value: <<<${defaultPort}>>>`);
+    console.log(`Using default value: <<<${getDefaultPort()}>>>`);
   }
-}
+};
 
-readConfig();
+main();
