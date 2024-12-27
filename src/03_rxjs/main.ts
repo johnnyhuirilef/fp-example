1feeaeb3f7b99d4732d48ae9d576ed48a76ab093
Johnny J. Huirilef Millán <johnnyhuirilef@gmail.com>
2024-12-27 09:29:36 -0300

    fix(rxjs): handle port configuration errors and log default port value

--- src/03_rxjs/main.ts
+++ src/03_rxjs/main.ts
@@ -1,9 +1,13 @@
 import { getPortFromConfig$ } from "./services/port-config.service";
+import { getDefaultPort } from "../shared";
 
 const bootstrap = () => {
   getPortFromConfig$().subscribe({
     next: (port) => console.log(`Configured port is: <<<${port}>>>`),
-    error: (error) => console.log("Error reading file:", error),
+    error: (error) => {
+      console.log("Error reading file:", error);
+      console.log(`Configured port is: <<<${getDefaultPort()}>>>`);
+    },
   });
 };
 
